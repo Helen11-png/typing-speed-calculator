32b02b12d09fa5ca8cab81b27059e34e7adf6aa0
alena <alena.nik.07@mail.ru>
2026-03-04 11:27:59 +0300

    training

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+	"time"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println("Введите строку")
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	a := scanner.Text()
+	fmt.Printf("%d\n", strings.Count(a, " "))
+	for j := 0; j < strings.Count(a, " ")+1; j++ {
+		for i := 0; i < len(a); i++ {
+			if string(a[i]) != " " {
+				fmt.Print(string(a[i]))
+			} else {
+				fmt.Println()
+				fmt.Print("\033[H\033[2J")
+			}
+			time.Sleep(2000 * time.Millisecond)
+		}
+	}
 }
